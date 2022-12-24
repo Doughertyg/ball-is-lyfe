2038f29635d4533b61da24169dc003993e47fc45
Graham Dougherty <6759552+Doughertyg@users.noreply.github.com>
2022-12-24 00:25:27 -0800

    Update webpack.config.js
    update webpack config for new index.jsx file. rebase on master

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ var DEST_DIR = path.join(__dirname, "/public");
 var TerserPlugin = require("terser-webpack-plugin");
 
 module.exports = {
-  entry: `${SRC_DIR}/index.js`,
+  entry: `${SRC_DIR}/index.jsx`,
   output: {
     filename: "bundle.js",
     path: DEST_DIR,
